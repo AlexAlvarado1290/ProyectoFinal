910f624293b4e252a5fdb82d7eacd8f1e7ad188d
AlexAlvarado1290 <125413909+AlexAlvarado1290@users.noreply.github.com>
2024-11-01 21:46:58 -0600

    agregar proyecto

---
+++ src/main/java/com/mycompany/proyectofinprogra/ProyectoFinProgra.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinprogra;
+
+/**
+ *
+ * @author joel2
+ */
+public class ProyectoFinProgra {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    RegistroNuevoEvent principal = new RegistroNuevoEvent();
+    principal.show();
+    }
+}
